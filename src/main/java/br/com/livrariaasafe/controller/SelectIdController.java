a5e6a9465f54f455fb6e217740b2843f465b338d
pbarrios <pbarrios@magnasistemas.com.br>
2023-03-03 18:49:46 -0300

    busca por id adicionada com JPA

--- src/main/java/br/com/livrariaasafe/controller/SelectIdController.java
+++ src/main/java/br/com/livrariaasafe/controller/SelectIdController.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.livrariaasafe.model.Book;
+import br.com.livrariaasafe.model.BookDAO;
 import br.com.livrariaasafe.model.DAO;
 import br.com.livrariaasafe.model.JavaBeans;
 
@@ -37,9 +39,12 @@ public class SelectIdController extends HttpServlet {
 
 	protected void showBookId(HttpServletRequest request, HttpServletResponse response)
 			throws IOException, ServletException {
+		BookDAO bookDAO = new BookDAO();
 		String id = request.getParameter("id");
-		JavaBeans javaBeans = dao.showBookId(id);
-		request.setAttribute("javabeans", javaBeans);
+		Long idLong = Long.parseLong(id);
+//		JavaBeans javaBeans = dao.showBookId(id);
+		Book bookJavaBeans = bookDAO.selectId(idLong);
+		request.setAttribute("javabeans", bookJavaBeans);
 		request.getRequestDispatcher("html/book-id.jsp").forward(request, response);
 	}
 
