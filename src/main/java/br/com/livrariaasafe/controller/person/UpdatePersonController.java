67fa7583b94ef4afc3ff0a3696e082bd744bdba2
pbarrios <pbarrios@magnasistemas.com.br>
2023-03-09 10:03:36 -0300

    classe UpdatePersonController refatorado, metodos doPost e doGet mais limpos e legiveis

--- src/main/java/br/com/livrariaasafe/controller/person/UpdatePersonController.java
+++ src/main/java/br/com/livrariaasafe/controller/person/UpdatePersonController.java
@@ -17,56 +17,62 @@ import br.com.livrariaasafe.model.person.PersonDAO;
 
 @WebServlet(urlPatterns = { "/UpdatePersonController" })
 public class UpdatePersonController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public UpdatePersonController() {
-		super();
-	}
+    public UpdatePersonController() {
+        super();
+    }
 
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		try {
-			Long idPerson = Long.parseLong(request.getParameter("id"));
-			PersonDAO personDAO = new PersonDAO();
-			Person person = personDAO.getPerson(idPerson);
-			BookDAO bookDAO = new BookDAO();
-			List<Book> books = bookDAO.readAllBooks();
-			request.setAttribute("nome", person.getName());
-			request.setAttribute("sobrenome", person.getSurname());
-			request.setAttribute("livro", person.getBook().getName());
-			request.setAttribute("person", person);
-			request.setAttribute("books", books);
-			RequestDispatcher requestDispatcher = request.getRequestDispatcher("html/edit-person.jsp");
-			requestDispatcher.forward(request, response);
-		} catch (ServletException | IOException e) {
-			e.printStackTrace();
-		}
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            Person person = getPersonById(request);
+            BookDAO bookDAO = new BookDAO();
+            List<Book> books = bookDAO.readAllBooks();
+            request.setAttribute("nome", person.getName());
+            request.setAttribute("sobrenome", person.getSurname());
+            request.setAttribute("livro", person.getBook().getName());
+            request.setAttribute("person", person);
+            request.setAttribute("books", books);
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("html/edit-person.jsp");
+            requestDispatcher.forward(request, response);
+        } catch (ServletException | IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	}
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            PersonDAO personDAO = new PersonDAO();
+            Person person = createPersonFromRequest(request);
+            personDAO.updatePerson(person);
+            response.sendRedirect("html/successfully-updated-person.jsp");
+        } catch (IOException | NumberFormatException e) {
+            e.printStackTrace();
+        }
+    }
 
-	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		try {
-			BookDAO bookDAO = new BookDAO();
-			PersonDAO personDAO = new PersonDAO();
-			Person person = new Person();
-			Long idPerson = Long.parseLong(request.getParameter("id"));
-			String name = request.getParameter("nome");
-			String surname = request.getParameter("sobrenome");
-			Long bookId = Long.parseLong(request.getParameter("livro"));
-			Book book = bookDAO.selectId(bookId);
-			person.setId(idPerson);
-			person.setName(name);
-			person.setSurname(surname);
-			person.setBook(book);
-			personDAO.updatePerson(person);
-			response.sendRedirect("html/successfully-updated-person.jsp");
+    private Person getPersonById(HttpServletRequest request) {
+        Long idPerson = Long.parseLong(request.getParameter("id"));
+        PersonDAO personDAO = new PersonDAO();
+        return personDAO.getPerson(idPerson);
+    }
 
-		} catch (IOException | NumberFormatException e) {
-			e.printStackTrace();
-		}
-	}
-
+    private Person createPersonFromRequest(HttpServletRequest request) {
+        BookDAO bookDAO = new BookDAO();
+        Person person = new Person();
+        Long idPerson = Long.parseLong(request.getParameter("id"));
+        String name = request.getParameter("nome");
+        String surname = request.getParameter("sobrenome");
+        Long bookId = Long.parseLong(request.getParameter("livro"));
+        Book book = bookDAO.selectId(bookId);
+        person.setId(idPerson);
+        person.setName(name);
+        person.setSurname(surname);
+        person.setBook(book);
+        return person;
+    }
 }
