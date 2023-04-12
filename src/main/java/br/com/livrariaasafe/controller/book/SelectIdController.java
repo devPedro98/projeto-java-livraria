8e5b5236c5f31feadf538b0e15a9df8bd98ac5a7
devPedro98 <pedroribeirobarrios@gmail.com>
2023-04-12 18:26:46 -0300

    taglib core adicionada ao pom, SelectIdController refatorado

--- src/main/java/br/com/livrariaasafe/controller/book/SelectIdController.java
+++ src/main/java/br/com/livrariaasafe/controller/book/SelectIdController.java
@@ -1,6 +1,8 @@
 package br.com.livrariaasafe.controller.book;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,37 +13,26 @@ import javax.servlet.http.HttpServletResponse;
 import br.com.livrariaasafe.model.book.Book;
 import br.com.livrariaasafe.model.book.BookDAO;
 
-@WebServlet(urlPatterns = { "/SelectIdController", "/selectid" })
+@WebServlet("/selectid")
 public class SelectIdController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-	public SelectIdController() {
-		super();
-
-	}
+	private final Logger logger = Logger.getLogger(SelectIdController.class.getName());
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		try {
-			String action = request.getServletPath();
-			if (action.equals("/selectid")) {
-				showBookId(request, response);
-			}
-		} catch (ServletException | IOException e) {
+			BookDAO bookDAO = new BookDAO();
+			String id = request.getParameter("id");
+			Long idLong = Long.parseLong(id);
+			Book bookJavaBeans = bookDAO.selectId(idLong);
+			request.setAttribute("javabeans", bookJavaBeans);
+			request.getRequestDispatcher("html/book-id.jsp").forward(request, response);
+
+		} catch (ServletException | IOException | NumberFormatException e) {
 			e.printStackTrace();
 		}
 
-	}
-
-	protected void showBookId(HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException {
-		BookDAO bookDAO = new BookDAO();
-		String id = request.getParameter("id");
-		Long idLong = Long.parseLong(id);
-		Book bookJavaBeans = bookDAO.selectId(idLong);
-		request.setAttribute("javabeans", bookJavaBeans);
-		request.getRequestDispatcher("html/book-id.jsp").forward(request, response);
 	}
 
 }
